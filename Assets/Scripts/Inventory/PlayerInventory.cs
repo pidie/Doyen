1f385f0f291a02196897b8cc626405c585eb6f52
Matt Burns <mmburns88@gmail.com>
2022-06-06 12:00:41 -0400

    created a visual PlayerInventory list for the inspector

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -5,7 +5,13 @@ namespace Inventory
 {
 	public class PlayerInventory : MonoBehaviour
 	{
+		[SerializeField] private List<CollectableData> window; // useful until Items can be made non-static
 		public static List<CollectableData> Items = new ();
+
+		private void Update()
+		{
+			window = Items;
+		}
 
 		public static void AddItem(CollectableData collectableData)
 		{
