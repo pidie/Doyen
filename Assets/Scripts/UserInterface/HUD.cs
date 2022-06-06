e4585a437c27e787dbb3ab5d342a2c90847b875e
Matt Burns <mmburns88@gmail.com>
2022-06-06 11:22:12 -0400

    enabled toggling of InventoryPanel

--- Assets/Scripts/UserInterface/HUD.cs
+++ Assets/Scripts/UserInterface/HUD.cs
@@ -5,15 +5,21 @@ namespace UserInterface
 {
     public class HUD : MonoBehaviour
     {
+        private static GameObject _inventoryPanel;
         private static GameObject _messageBoxPanel;
         private static TMP_Text _messageBox;
 
         private void Awake()
         {
+            _inventoryPanel = GameObject.Find("InventoryPanel");
+            _inventoryPanel.SetActive(false);
+            
             _messageBoxPanel = GameObject.Find("MessageBoxPanel");
             _messageBox = _messageBoxPanel.GetComponentInChildren<TMP_Text>();
             _messageBoxPanel.SetActive(false);
         }
+
+        public static void ToggleInventoryPanel() => _inventoryPanel.SetActive(!_inventoryPanel.activeSelf);
 
         public static void DisplayMessageBox(string message = "")
         {
