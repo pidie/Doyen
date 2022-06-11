ad790a94aa6f3504ed66736db21dee7d78fef031
Matt Burns <mmburns88@gmail.com>
2022-06-11 02:37:03 -0400

    added tooltip functionality to the inventory screen

--- Assets/Scripts/UserInterface/InventoryItemSlot.cs
+++ Assets/Scripts/UserInterface/InventoryItemSlot.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +8,10 @@ namespace UserInterface
 {
     public class InventoryItemSlot : MonoBehaviour
     {
+        [SerializeField] private GameObject UITooltip;
+
+        private GameObject _activeTooltip;
+        
         public RawImage icon;
         public TMP_Text quantityText;
         public string itemName;
