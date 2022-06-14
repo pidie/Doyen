66e3632c5a275d1f6bd9fae14a460f92154faf5b
Matt Burns <mmburns88@gmail.com>
2022-06-13 21:19:52 -0400

    changed ItemPickUp to prioritize based on proximity to player position

--- Assets/Scripts/Inventory/PickUpRadius.cs
+++ Assets/Scripts/Inventory/PickUpRadius.cs
@@ -12,6 +12,7 @@ namespace Inventory
         [SerializeField] private List<Collectable> collectablesInRange = new ();
 
         private SphereCollider _detectionSphere;
+        private Collectable _targetCollectable;
 
         public static Action itemPickUpRequested;
 
@@ -23,12 +24,14 @@ namespace Inventory
             itemPickUpRequested += TryPickUpItem;
         }
 
+        private void Update() => DetermineTargetCollectable();
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Collectable"))
             {
-                collectablesInRange.Add(other.GetComponent<Collectable>());
-                HUD.DisplayMessageBox($"{collectablesInRange.LastOrDefault().Data.name}\nPress {Globals.GetKeyBinding("Interact")} to pick up");
+                var collectable = other.GetComponent<Collectable>();
+                collectablesInRange.Add(collectable);
             }
         }
 
@@ -36,24 +39,53 @@ namespace Inventory
         {
             if (other.CompareTag("Collectable"))
             {
-                collectablesInRange.Remove(other.GetComponent<Collectable>());
-                if (collectablesInRange.Count < 1)
-                    HUD.HideMessageBox();
+                var collectable = other.GetComponent<Collectable>();
+                collectablesInRange.Remove(collectable);
+                CheckForHideMessageBox();
             }
         }
         
         public void TryPickUpItem()
         {
-            var item = collectablesInRange.LastOrDefault();
+            var item = _targetCollectable;
 
             if (item != null)
             {
                 PlayerInventory.AddItem(item.Data);
                 Destroy(item.gameObject);
                 collectablesInRange.Remove(item);
+                CheckForHideMessageBox();
+            }
+        }
+
+        private void CheckForHideMessageBox()
+        {
+            if (collectablesInRange.Count < 1)
+                HUD.HideMessageBox();
+        }
+
+        private void ShowMessageBox(Collectable collectable)
+        {
+            _targetCollectable = collectable;
+            HUD.DisplayMessageBox($"{collectable.Data.name}\nPress {Globals.GetKeyBinding("Interact")} to pick up");
+        }
+
+        private void DetermineTargetCollectable()
+        {
+            if (collectablesInRange.Count > 0)
+            {
+                foreach (var collectable in collectablesInRange.Where(collectable => collectable != _targetCollectable))
+                {
+                    if (_targetCollectable == null)
+                        ShowMessageBox(collectable);
+
+                    var currentTargetDistance = Vector3.Distance(transform.position, _targetCollectable.transform.position);
+                    var potentialTargetDistance = Vector3.Distance(transform.position, collectable.transform.position);
+
+                    if (potentialTargetDistance < currentTargetDistance)
+                        ShowMessageBox(collectable);
+                }
             }
         }
     }
 }
-
-
