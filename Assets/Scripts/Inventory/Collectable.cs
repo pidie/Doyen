5e6a1b622b64d13e9e5099b3085450a0fa24899c
Matt Burns <mmburns88@gmail.com>
2022-06-19 16:59:50 -0400

    Created a spawn point for ingredients

--- Assets/Scripts/Inventory/Collectable.cs
+++ Assets/Scripts/Inventory/Collectable.cs
@@ -2,73 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Inventory
 {
-    // todo : start to distinguish a collectable from explicitly being an ingredient
     [RequireComponent(typeof(Collider))]
-    public class Collectable : MonoBehaviour
+    public abstract class Collectable : MonoBehaviour
     {
-        [SerializeField] private IngredientData ingredientData;
-        [SerializeField] private List<IngredientPropertyValue> properties;
+        protected Collider col;
+        protected Outline outline;
         
-        private Collider _collider;
-        private Outline _outline;
-        
-        public bool IsAutocollected { get; private set; }
-        public CollectableData Data
-        {
-            get
-            {
-                var isActiveAtCall = true;
-                
-                if (!gameObject.activeSelf)
-                {
-                    isActiveAtCall = false;
-                    gameObject.SetActive(true);
-                }
-                
-                var data = new CollectableData(ingredientData.name, ingredientData, properties);
-                
-                if (!isActiveAtCall) 
-                    gameObject.SetActive(false);
-
-                return data;
-            }
-        }
+        public bool IsAutocollected { get; protected set; }
+        public virtual CollectableData Data { get; set; }
 
         private void Awake()
         {
-            _outline = GetComponent<Outline>();
-            _outline.enabled = false;
-            _collider = GetComponent<Collider>();
-            _collider.isTrigger = true;
+            outline = GetComponent<Outline>();
+            outline.enabled = false;
+            col = GetComponent<Collider>();
+            col.isTrigger = true;
 
             IsAutocollected = false;
         }
 
-        private void OnEnable()
-        {
-            var defaultProperties = new[]
-            {
-                ingredientData.purity, 
-                ingredientData.lifeSpan
-            };
+        public void ToggleOutlineOn() => outline.enabled = true;
 
-            var allProperties = defaultProperties.Concat(ingredientData.properties);
-            
-            foreach (var property in allProperties)
-            {
-                var name = property.name;
-                var value = Random.Range(property.minimum, property.maximum);
-                properties.Add(new IngredientPropertyValue(name, value));
-            }
-        }
-
-        public void ToggleOutlineOn() => _outline.enabled = true;
-
-        public void ToggleOutlineOff() => _outline.enabled = false;
+        public void ToggleOutlineOff() => outline.enabled = false;
     }
 
     [Serializable]
---
+++ Assets/Scripts/Inventory/Ingredient.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Inventory
+{
+	public class Ingredient : Collectable
+	{
+		[SerializeField] private IngredientData ingredientData;
+		[SerializeField] private List<IngredientPropertyValue> properties;
+        
+		public override CollectableData Data
+		{
+			get
+			{
+				var isActiveAtCall = true;
+                
+				if (!gameObject.activeSelf)
+				{
+					isActiveAtCall = false;
+					gameObject.SetActive(true);
+				}
+                
+				var data = new CollectableData(ingredientData.name, ingredientData, properties);
+                
+				if (!isActiveAtCall) 
+					gameObject.SetActive(false);
+
+				return data;
+			}
+		}
+        
+		private void OnEnable()
+		{
+			var defaultProperties = new[]
+			{
+				ingredientData.purity, 
+				ingredientData.lifeSpan
+			};
+
+			var allProperties = defaultProperties.Concat(ingredientData.properties);
+            
+			foreach (var property in allProperties)
+			{
+				var name = property.name;
+				var value = Random.Range(property.minimum, property.maximum);
+				properties.Add(new IngredientPropertyValue(name, value));
+			}
+		}
+	}
+}
