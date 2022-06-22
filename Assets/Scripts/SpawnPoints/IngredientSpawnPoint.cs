66ffdc9b46fe0f993c6eac65436b90c6009abde8
Matt Burns <mmburns88@gmail.com>
2022-06-22 10:52:30 -0400

    made the spawners active in edit mode - disable/enable the script to reset

--- Assets/Scripts/SpawnPoints/IngredientSpawnPoint.cs
+++ Assets/Scripts/SpawnPoints/IngredientSpawnPoint.cs
@@ -1,5 +1,4 @@
 using System;
-using Inventory;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -13,25 +12,38 @@ namespace SpawnPoints
 		Plentiful,
 		Packed
 	}
+	[ExecuteInEditMode]
 	public class IngredientSpawnPoint : MonoBehaviour
 	{
 		[SerializeField] private GameObject ingredient;
 		[SerializeField] private GameObject node;
 		[SerializeField] private SpawnPointDensity density = SpawnPointDensity.Typical;
+		[SerializeField] private float distanceBetweenNodes = 1f;
 		
 		private int _spawnerDiameter;
 		private Vector3[] _nodePositions;
 		private float _densityMultiplier;
 
-		private void Awake()
+		private void OnEnable()
 		{
 			_spawnerDiameter = (int) transform.localScale.x;
 			SetDensity();
 			_nodePositions = new Vector3[(int) Math.Round(_spawnerDiameter * _densityMultiplier)];
 			gameObject.name = $"{Globals.TitleCase(ingredient.name)} Spawner";
+			PlaceNodes();
 		}
 
-		private void Start() => PlaceNodes();
+		private void OnDisable()
+		{
+			foreach (var child in GetComponentsInChildren<IngredientNode>())
+			{
+				#if UNITY_EDITOR
+				child.Destroy();
+				#else
+				Destroy(child.gameObject);
+				#endif
+			}
+		}
 
 		private void SetDensity()
 		{
@@ -60,8 +72,8 @@ namespace SpawnPoints
 				
 				var nodeLocation = Random.insideUnitSphere * _spawnerDiameter * 0.5f;
 				nodeLocation += transform.position;
-				if (!Physics.Raycast(nodeLocation + new Vector3(0, -1, 0), Vector3.up, out var hit, _spawnerDiameter))
-					if (!Physics.Raycast(nodeLocation + new Vector3(0, 1, 0), Vector3.down, out hit, _spawnerDiameter))
+				if (!Physics.Raycast(nodeLocation + new Vector3(0, -1, 0), Vector3.up, out var hit, _spawnerDiameter + 1))
+					if (!Physics.Raycast(nodeLocation + new Vector3(0, 1, 0), Vector3.down, out hit, _spawnerDiameter + 1))
 					{
 						i--;
 						continue;
@@ -71,7 +83,7 @@ namespace SpawnPoints
 
 				for (var j = i - 1; j >= 0; j--)
 				{
-					if (Vector3.Distance(nodeLocation, _nodePositions[j]) < 1)
+					if (Vector3.Distance(nodeLocation, _nodePositions[j]) < distanceBetweenNodes)
 					{
 						resetNodePlacement = true;
 						break;
@@ -83,11 +95,11 @@ namespace SpawnPoints
 			}
 
 			var count = 1;
-			foreach (var location in _nodePositions)
+			foreach (var position in _nodePositions)
 			{
-				var newNode = Instantiate(node, location, Quaternion.identity, transform);
+				var newNode = Instantiate(node, position, Quaternion.identity, transform);
+				newNode.GetComponent<MeshRenderer>().enabled = false;
 				var n = newNode.AddComponent<IngredientNode>();
-				n.GetComponent<MeshRenderer>().enabled = false;
 				n.ingredient = ingredient;
 				n.SpawnIngredient();
 				var scaling = node.transform.localScale.x / _spawnerDiameter;
@@ -97,17 +109,29 @@ namespace SpawnPoints
 			}
 		}
 
+		#if UNITY_EDITOR
 		private void OnDrawGizmos()
 		{
+			var radius = transform.localScale.x * 0.5f;
 			Gizmos.color = Color.cyan;
-			Gizmos.DrawWireSphere(transform.position, _spawnerDiameter / 2f);
+			Gizmos.DrawWireSphere(transform.position, radius);
 		}
+
+		private void OnDrawGizmosSelected()
+		{			
+			var radius = transform.localScale.x * 0.5f;
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(transform.position, radius);
+		}
+		#endif
 	}
 
 	public class IngredientNode : MonoBehaviour
 	{
 		public GameObject ingredient;
 		public bool hasBeenCollected;
+
+		public void Destroy() => DestroyImmediate(gameObject);
 
 		public void SpawnIngredient()
 		{
