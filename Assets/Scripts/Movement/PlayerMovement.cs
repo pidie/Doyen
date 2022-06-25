79c8779c3b6f29c3927fa2f78a6df0937bde15a5
Matt Burns <mmburns88@gmail.com>
2022-06-25 07:16:49 -0400

    applied jump for the male and female

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Movement
@@ -15,14 +16,21 @@ namespace Movement
         private Animator _animator;
         private Vector3 _gravVelocity;
         private int _runBlendHash;
+        private int _isJumpingHash;
+        private bool _hasJumped;
 
         private const float Gravity = -9.81f;
+
+        public static Action onLanding;
 
         private void Awake()
         {
             _controller = GetComponent<CharacterController>();
             _animator = GetComponentInChildren<Animator>();
             _runBlendHash = Animator.StringToHash("Run Blend");
+            _isJumpingHash = Animator.StringToHash("isJumping");
+
+            onLanding += Land;
         }
 
         private void Update()
@@ -42,7 +50,6 @@ namespace Movement
 
             _controller.Move(transform.rotation * moveVelocity);
             
-            // var runBlend = moveVelocity.z < 0.05f ? 0 : isRunning ? 2 : vertical < 0f ? -1 : 1;
             var runBlend = 0;
 
             if (Mathf.Abs(moveVelocity.z) > 0)
@@ -51,14 +58,21 @@ namespace Movement
                 runBlend *= 2;
             if (vertical < 0)
                 runBlend *= -1;
+            if (vertical < 0 && isRunning)
+                moveVelocity.z++;
             
             _animator.SetFloat(_runBlendHash, runBlend, 0.1f, Time.deltaTime);
 
             if (Input.GetButtonDown("Jump") && groundCheck.isGrounded)
+            {
+                _animator.SetBool(_isJumpingHash, true);
                 _gravVelocity.y = Mathf.Sqrt(jumpHeight * -2f * Gravity);
+            }
             
             _gravVelocity.y += Gravity * gravitationalForce * Time.deltaTime;
             _controller.Move(_gravVelocity * Time.deltaTime);
         }
+
+        private void Land() => _animator.SetBool(_isJumpingHash, false);
     }
 }
